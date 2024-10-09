5beb8b4110833baf2e0f4969c82423a4a0671cfb
ConnerWang <hzla709515@gmail.com>
2024-10-09 11:58:20 -0400

    fixed bug

--- script.js
+++ script.js
@@ -2,3 +2,29 @@ import './style.scss'
 import './src/footer.scss'
 import './src/header.scss'
 import './src/foundation.scss'
+
+let lightmode = localStorage.getItem('lightmode');
+const themeSwitch = document.getElementById('theme-switch');
+
+const enableLightmode = () => {
+    document.body.classList.add('lightmode');
+    localStorage.setItem('lightmode', 'active');
+};
+
+const disableLightmode = () => {
+    document.body.classList.remove('lightmode');
+    localStorage.setItem('lightmode', null);
+};
+
+if (lightmode === 'active') enableLightmode();
+
+themeSwitch.addEventListener("click", () => {
+    lightmode = localStorage.getItem('lightmode');
+    if (lightmode !== "active") {
+        console.log("Enabling light mode");
+        enableLightmode();
+    } else {
+        console.log("Disabling light mode");
+        disableLightmode();
+    }
+});
