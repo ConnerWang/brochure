8be1f3d1f74c7d02e2a22bef6e1833d94b1bc20b
ConnerWang <hzla709515@gmail.com>
2024-10-08 23:50:38 -0500

    dark mode switch

--- script.js
+++ script.js
@@ -2,3 +2,23 @@ import './style.scss'
 import './src/footer.scss'
 import './src/header.scss'
 import './src/foundation.scss'
+
+let button = document.querySelector('.theme-switch');
+
+
+button.addEventListener('click',function(){
+
+    let currentTheme = document.querySelector('html').getAttribute('data-theme')
+    if (currentTheme === 'light'){
+
+        document.querySelector('html').setAttribute('data-theme','dark')
+        sessionStorage.setItem('selectedTheme','dark')
+    }else{
+        document.querySelector('html').setAttribute('data-theme','light')
+        sessionStorage.setItem('selectedTheme','light')
+    }
+})
+
+if(sessionStorage.getItem('selectedTheme')){
+    document.querySelector('html').setAttribute('data-theme',sessionStorage.getItem('selectedTheme'))
+}
